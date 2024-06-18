939bb7c68e539627a003715d4b2de409931954fd
wayne <168912749+wayne9958@users.noreply.github.com>
2024-06-18 15:08:55 +0800

    Add files via upload

---
+++ js/99.e839df73.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkv"]=self["webpackChunkv"]||[]).push([[99],{99:function(n,u,l){l.r(u),l.d(u,{default:function(){return h}});var r=l(641);const e={class:"about"},t=(0,r.Lk)("h1",null,"嗨我是莊詠惟，畢業於靜宜大學資訊管理學系，目前正致力於研究vue.js，react.js。",-1),s=(0,r.Lk)("br",null,null,-1),c=(0,r.Lk)("h1",null,null,-1),a=[t,s,c];function k(n,u){return(0,r.uX)(),(0,r.CE)("div",e,a)}var v=l(262);const f={},o=(0,v.A)(f,[["render",k]]);var h=o}}]);
+//# sourceMappingURL=99.e839df73.js.map
