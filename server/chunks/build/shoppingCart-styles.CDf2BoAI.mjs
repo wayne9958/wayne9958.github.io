e6ebdd8c14cc8ced76f651264d85b35e20c940ff
wayne <168912749+wayne9958@users.noreply.github.com>
2026-01-07 14:57:33 +0800

    0107
    nuxt test

--- server/chunks/build/shoppingCart-styles.CDf2BoAI.mjs
+++
@@ -1,8 +0,0 @@
-const shoppingCart_vue_vue_type_style_index_0_scoped_98f0f20a_lang = "";
-
-const shoppingCartStyles_CDf2BoAI = [
-  shoppingCart_vue_vue_type_style_index_0_scoped_98f0f20a_lang
-];
-
-export { shoppingCartStyles_CDf2BoAI as default };
-//# sourceMappingURL=shoppingCart-styles.CDf2BoAI.mjs.map
---
+++ server/client.manifest.mjs
@@ -0,0 +1,1 @@
+export { default } from "file:///C:/Users/USER/Desktop/front/profilenuxt/node_modules/@nuxt/vite-builder/dist/runtime/client.manifest.mjs"
